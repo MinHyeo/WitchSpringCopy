a77784cfe6debca29ef58dd2b9e1fc5167edd06e
L-Ju123 <naver514411@gmail.com>
2024-08-05 17:16:22 +0900

    Move
    2024.08.05
    마녀 움직임까지 구현

---
+++ WitchSpring/Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Define.CameraMode _mode = Define.CameraMode.QuaterView;
+
+    [SerializeField]
+    Vector3 _delta = new Vector3(0.4f, 1.2f, -1.2f); //플레이어 기준으로 떨어진 거리
+
+    [SerializeField]
+    GameObject _player = null;
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        if (_mode == Define.CameraMode.QuaterView)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_player.transform.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Wall")))
+            {
+                float dist = (hit.point - _player.transform.position).magnitude * 0.8f;
+                transform.position = _player.transform.position + _delta.normalized * dist;
+            }
+            else
+            {
+                transform.position = _player.transform.position + _delta;
+                transform.LookAt(_player.transform);
+
+            }
+
+        }
+    }
+
+    public void SetQuaterView(Vector3 delta)
+    {
+        _mode = Define.CameraMode.QuaterView;
+        _delta = delta;
+    }
+}
---
+++ WitchSpring/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10.0f;
+
+    Vector3 _destPos;
+
+    void Start()
+    {
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
+    }
+
+
+    public enum PlayerState
+    {
+        Die,
+        Moving,
+        Idle,
+        Battle,
+    }
+
+    PlayerState _state = PlayerState.Idle;
+
+   
+    void UpdateMoving()
+    {
+        Vector3 dir = _destPos - transform.position;
+        if (dir.magnitude < 0.0001f) 
+        {
+            _state = PlayerState.Idle;
+        }
+        else
+        {
+            float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+            transform.position += dir.normalized * moveDist;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+        }
+
+        Animator anim = GetComponent<Animator>();
+
+        anim.SetFloat("speed", _speed);
+    }
+
+
+    void UpdateIdle()
+    {
+        Animator anim = GetComponent<Animator>();
+        anim.SetFloat("speed", 0);
+
+    }
+
+    void UpdateBattle()
+    {
+
+        _state = PlayerState.Battle;
+        Animator anim = GetComponent<Animator>();
+        anim.SetBool("battle", true);
+    }
+
+
+    void Update()
+    {
+
+        switch (_state)
+        {
+            case PlayerState.Moving:
+                UpdateMoving();
+                break;
+            case PlayerState.Idle:
+                UpdateIdle();
+                break;
+            case PlayerState.Battle:
+                UpdateBattle();
+                break;
+
+        }
+
+    }
+
+
+    void OnMouseClicked(Define.MouseEvent evt)
+    {
+        if (_state == PlayerState.Die || _state == PlayerState.Battle) return;
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        {
+            _destPos = hit.point;
+            _state = PlayerState.Moving;
+        }
+    }
+}
