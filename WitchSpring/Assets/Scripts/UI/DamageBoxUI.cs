0558e6a420aa03764914ba035912502937388fe9
swatper <67682393+swatper@users.noreply.github.com>
2024-08-17 21:12:38 +0900

    데미지 표현 구현 완료

--- WitchSpring/Assets/Scripts/UI/DamageBoxUI.cs
+++ WitchSpring/Assets/Scripts/UI/DamageBoxUI.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DamageBoxUI : MonoBehaviour
 {
-    
+    [SerializeField] List<Image> Num;
     void Start()
     {
-        
+    }
+
+    public void SetDamage(List<Sprite> number) {
+        for (int i = 0; i < number.Count; i++) {
+            Num[i].gameObject.SetActive(true);
+            Num[i].sprite = number[i];
+        }
     }
 
     public void DestroyBox() {
